651da96dd0a8acb196ac0f5fa59ce2eff6b08994
Valentino <valentinoindorato@gmail.com>
2023-10-20 20:39:04 -0300

    cambio en app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './styles/main.scss'
 import { BrowserRouter } from 'react-router-dom'
 
