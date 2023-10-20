79c6bba97f6802c2c44b2de4277c3375b93d2c51
Valentino <valentinoindorato@gmail.com>
2023-10-20 20:17:09 -0300

    correccion base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/presupuesto-prueba/'
+  base:'https://github.com/ValentinoIndorato/presupuesto-prueba/'
 })
